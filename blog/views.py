ee4bc581946f689dcae5b6ea8db393c4e0d24cd3
Your Name <you@example.com>
2025-07-09 13:47:38 +0530

    Change FBV of post_list into CBV PostListView using Django builtin generic Views

--- blog/views.py
+++ blog/views.py
@@ -7,22 +7,29 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 #     return render(request, 'blog/list.html', {'posts': posts})
 # 
 
-def post_list(request):
-    object_list = Post.published.all()
-    paginator = Paginator(object_list, 3) # 3 posts in each page
-    page = request.GET.get('page')
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
- # If page is not an integer deliver the first page
-        posts = paginator.page(1)
-    except EmptyPage:
- # If page is out of range deliver last page of results
-        posts = paginator.page(paginator.num_pages)
-    return render(request,
-                  'blog/list.html',
-                   {'page': page,
-                    'posts': posts})
+# def post_list(request):
+#     object_list = Post.published.all()
+#     paginator = Paginator(object_list, 3) # 3 posts in each page
+#     page = request.GET.get('page')
+#     try:
+#         posts = paginator.page(page)
+#     except PageNotAnInteger:
+#  # If page is not an integer deliver the first page
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#  # If page is out of range deliver last page of results
+#         posts = paginator.page(paginator.num_pages)
+#     return render(request,
+#                   'blog/list.html',
+#                    {'page': page,
+#                     'posts': posts})
+
+from django.views.generic import ListView
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/list.html'
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
