a096be04b3b2cfa19041469ecf2d2dc7648dc3e7
Your Name <you@example.com>
2025-07-09 15:18:37 +0530

    Enable email option to Send a Post to read

--- blogApp_django3/settings.py
+++ blogApp_django3/settings.py
@@ -121,3 +121,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'guhan3697figmentmajesty@gmail.com'
+EMAIL_HOST_PASSWORD = 'jxun tzuu gvfg acqo'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
