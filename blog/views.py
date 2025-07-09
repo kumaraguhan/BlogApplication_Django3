57e1abec9c6d6102520e134d5b53fdce993ad63b
Your Name <you@example.com>
2025-07-09 16:26:54 +0530

    Enable Comment Functionality for User to Comment on a Post
    Created a Comment Model, Comments will be shown in detail page of a Post all comments will be available and a form to add a new comment

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # def post_list(request):
@@ -37,12 +37,32 @@ def post_detail(request, year, month, day, post):
                                    publish__year=year,
                                    publish__month=month,
                                    publish__day=day)
+    
+     # List of active comments for this post
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+    # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+        # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+        # Assign the current post to the comment
+            new_comment.post = post
+        # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
     return render(request,
                   'blog/detail.html',
-                  {'post': post})
+                  {'post': post,
+                   'comments' : comments,
+                    'new_comment': new_comment,
+                     'comment_form': comment_form, })
 
 from django.core.mail import send_mail
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 def post_share(request, post_id):
     # Retrieve post by id
     post = get_object_or_404(Post, id=post_id, status='published')
