fe1c1a7d3b62bc4086afccbe04115b3cf7511aa8
Your Name <you@example.com>
2025-07-10 17:25:51 +0530

    Add Full-text search to Blog using Postgres feauture
    Implemeted full fledged text search with simple lookup to multiple fields lookup with Stemming and ranking and trigram

--- blogApp_django3/settings.py
+++ blogApp_django3/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'taggit',
     'django.contrib.sites',
     'django.contrib.sitemaps',
+    'django.contrib.postgres',
 ]
 
 MIDDLEWARE = [
@@ -78,13 +79,21 @@ WSGI_APPLICATION = 'blogApp_django3.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog_db',
+        'USER': 'blog_user',
+        'PASSWORD': 'root',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
